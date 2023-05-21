1165a8de4161cb14d12c545d5cc656f0a13b7cdb
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-05-21 11:33:12 +0200

    Create ResultsResearch.jsx
    Project file

---
+++ src/components/ResultsResearch.jsx
@@ -0,0 +1,16 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import ProductResultContext from "../context/ProductResultContext";
+
+export default function ResultsResearch(props) {
+
+    const { productResult, setProductResult } = useContext(ProductResultContext);
+
+    return (
+        <>
+            <Link to="/product" className="resultsResearch" onClick={() => setProductResult(props.props)} >
+                <div className="resultsResearch__nameProduct">{props.props.generic_name}</div>
+            </Link>
+        </>
+    )
+}
