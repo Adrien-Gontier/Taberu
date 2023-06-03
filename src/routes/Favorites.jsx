a37399a0aa81233ac3dbca67dec8813a900fb044
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-06-03 10:22:10 +0200

    Update Favorites.jsx
    Format code improved

--- src/routes/Favorites.jsx
+++ src/routes/Favorites.jsx
@@ -1,15 +1,20 @@
 import React, { useContext } from 'react'
-import OneFavorites from '../components/OneFavorites';
-import FavoritesContext from '../context/FavoritesContext';
+import OneFavorites from '../components/OneFavorites'
+import FavoritesContext from '../context/FavoritesContext'
 
 export default function Favorites() {
-
-    const { favorites, setFavorites } = useContext(FavoritesContext);
+    const { favorites, setFavorites } = useContext(FavoritesContext)
 
     return (
         <div className="favorites">
-            <p className="favorites__title">Listes des produits mis en favories</p>
-            {favorites.map((oneFavoritesProduct) => <div key={oneFavoritesProduct.code}><OneFavorites props={oneFavoritesProduct} /></div>)}
+            <p className="favorites__title">
+                Listes des produits mis en favories
+            </p>
+            {favorites.map((oneFavoritesProduct) => (
+                <div key={oneFavoritesProduct.code}>
+                    <OneFavorites props={oneFavoritesProduct} />
+                </div>
+            ))}
         </div>
     )
 }
