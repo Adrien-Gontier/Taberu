02f9c58ac9ad33fed5a0c3bfbad5ed9edbda7424
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-06-03 10:22:06 +0200

    Update DoubleSearch.jsx
    Format code improved

--- src/routes/DoubleSearch.jsx
+++ src/routes/DoubleSearch.jsx
@@ -1,21 +1,26 @@
-import GlassIcon from "../assets/glass_magnifier_search_icon.svg";
-import { Link } from "react-router-dom";
-import NameNumberSearch from "../components/NameNumberSearch";
-import BarCodeSearch from "../components/BarCodeSearch";
+import GlassIcon from '../assets/glass_magnifier_search_icon.svg'
+import { Link } from 'react-router-dom'
+import NameNumberSearch from '../components/NameNumberSearch'
+import BarCodeSearch from '../components/BarCodeSearch'
 
 export default function DoubleSearch() {
+    const glassIcon = (
+        <img
+            className="searchResult__input-glassIcon__GlassIcon"
+            src={GlassIcon}
+            alt="Glass icon"
+        />
+    )
 
-  const glassIcon = <img className="searchResult__input-glassIcon__GlassIcon" src={GlassIcon} alt="Glass icon" />;
-
-  return (
-    <div className="doubleSearch">
-      <NameNumberSearch />
-      <div className="doubleSearch__searchByName">
-        <p>Pour fair une recherche par nom de produit</p>
-        <p>Cliquez sur la loupe</p>
-        <Link to="/searchresult">{glassIcon}</Link>
-      </div>
-      <BarCodeSearch />
-    </div>
-  )
+    return (
+        <div className="doubleSearch">
+            <NameNumberSearch />
+            <div className="doubleSearch__searchByName">
+                <p>Pour fair une recherche par nom de produit</p>
+                <p>Cliquez sur la loupe</p>
+                <Link to="/searchresult">{glassIcon}</Link>
+            </div>
+            <BarCodeSearch />
+        </div>
+    )
 }
