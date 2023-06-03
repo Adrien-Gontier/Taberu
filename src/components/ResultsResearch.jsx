f0b8650fd78d76a7731d4511a01b1c68eba92717
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-06-03 10:21:40 +0200

    Update ResultsResearch.jsx
    Format code improved

--- src/components/ResultsResearch.jsx
+++ src/components/ResultsResearch.jsx
@@ -1,15 +1,20 @@
-import { useContext } from "react";
-import { Link } from "react-router-dom";
-import ProductResultContext from "../context/ProductResultContext";
+import { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import ProductResultContext from '../context/ProductResultContext'
 
 export default function ResultsResearch(props) {
-
-    const { productResult, setProductResult } = useContext(ProductResultContext);
+    const { productResult, setProductResult } = useContext(ProductResultContext)
 
     return (
         <>
-            <Link to="/product" className="resultsResearch" onClick={() => setProductResult(props.props)} >
-                <div className="resultsResearch__nameProduct">{props.props.generic_name}</div>
+            <Link
+                to="/product"
+                className="resultsResearch"
+                onClick={() => setProductResult(props.props)}
+            >
+                <div className="resultsResearch__nameProduct">
+                    {props.props.generic_name}
+                </div>
             </Link>
         </>
     )
